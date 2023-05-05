945814efe7da35dad4d01ffb800d85a901d3209b
filecc <fil.gia@live.it>
2023-05-05 15:38:56 +0200

    added some style

--- index.php
+++ index.php
@@ -1,18 +1,21 @@
 <?php 
-include __DIR__ . '/partials/haed.php';
+include __DIR__ . '/partials/head.php';
 ?>
 
 
 <header>
-    <h1>Hello</h1>
+    <h1 class="text-white">Il magico generatore di</h1>
+    <div class="">
+    <img class="img-fluid p-2" src="./images/password.png" alt="password">
+    </div>
 </header>
 <main>
     <form action="results.php" method="GET">
         <div class="mb-3">
-            <label for="passwordLenght" class="form-label">Inserisci la lunghezza della password (max 30)</label>
+            <label for="passwordLenght" class="form-label text-white">Inserisci la lunghezza della password (max 30)</label>
             <input type="number" class="form-control" id="passwordLenght" name="passwordLenght" required>
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-warning">Submit</button>
     </form>
    
 </main>
--- results.php
+++ results.php
@@ -1,22 +1,40 @@
 <?php 
-include __DIR__ . '/partials/haed.php';
+include __DIR__ . '/partials/head.php';
 include __DIR__ . '/functions/start.php';
+$copied = false;
 generatePassword();
 ?>
+<main>
+
 
 <?php if($_SESSION['complete']): ?>
-    <h1>Ecco la tua bella password.</h1>
-        <p>
+    <div class="w-100 p-4">
+        <img class="img-fluid" aria-hidden="true" src="./images/eccola.png" alt="eccola">
+    </div>
+        <p id="generatedPassword" class="py-3 px-4 rounded bg-body-tertiary">
         <?php foreach ($_SESSION['password'] as $value)  {
             echo $value;
         } ?>
+        <span class="ms-2 d-none d-sm-inline"><i id="copy" class="bi bi-clipboard"></i></span>
         </p>
-        <a href="index.php">Torna alla home</a>
+        <a class="btn btn-warning" href="index.php">Torna alla home</a>
 <?php else: ?>
     <?php header('Location: ./index.php') ?>
     <?php endif ?>
 
-
+    </main>
+    <script>
+        const password = document.getElementById('generatedPassword');
+        const copyButton = document.getElementById('copy');
+        const handleCopy = function () {
+            navigator.clipboard.writeText(password.innerText);
+            copyButton.classList.remove('bi-clipboard');
+            copyButton.classList.add('bi-clipboard-check-fill');
+            copyButton.classList.add('text-success');
+        }
+        copyButton.addEventListener('click', handleCopy);
+      
+    </script>
 
 <?php 
 include __DIR__ . '/partials/footer.php';
